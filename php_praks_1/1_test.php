bb1edbb795d79c7a91584432991618ebff879e07
khkmihhail <mihhail.matisinets@khk.ee>
2018-01-30 10:29:44 +0200

    second test commit of the day

---
+++ php_praks_1/1_test.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>Some test strings</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mihhail.matisinets
+ * Date: 30.01.2018
+ * Time: 9:54
+ */
+
+echo "<h1>Hi, first string in php</h1>";
+
+$i = 0;
+$num = 40;
+
+while( $i < 10) {
+    $num--;
+    $i++;
+}
+
+echo ("i = $i and num = $num")
+
+?>
+</body>
+</html>
