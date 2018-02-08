38aa46aa5de49dfd007a9e9198fc35a2689656a0
khkmihhail <mihhail.matisinets@khk.ee>
2018-02-08 12:21:15 +0200

    Tehtud tunni ülesanne

---
+++ php_praks_1/ülesanne1_8_02.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mihhail.matisinets
+ * Date: 8.02.2018
+ * Time: 10:24
+ */
+//create a dictionary
+$menu = array(
+    array(
+      'nimetus' => 'snitsel',
+      'kirjeldus' => 'šnitsel sealihast, lisand, kaste, salat, leib',
+      'hind' => 2.68
+    ),
+    array(
+        'nimetus' => 'seapraad',
+        'kirjeldus' => 'seapraad, lisand, kaste, salat, leib',
+        'hind' => 2.65
+    ),
+    array(
+        'nimetus' => 'Hakklihapallid tomatikastmes',
+        'kirjeldus' => 'hakklihapall 2 tk, lisand, kaste, salat, leib',
+        'hind' => 2.30
+    ),
+    array(
+        'nimetus' => 'Hakklihapallid tomatikastmes 1/2',
+        'kirjeldus' => 'hakklihapall 1 tk, lisand, kaste, salat, leib',
+        'hind' => 1.30
+    ),
+);
+
+//made foreach loop
+foreach ($menu as $praed) {
+    printf('<br><b>Praed:</b> %s <b>Kirjeldus:</b> %s <b>Hind:</b> %s',
+        $praed['nimetus'],$praed['kirjeldus'],$praed['hind']
+    );
+};
+
+
+
+
+
+
+?>
