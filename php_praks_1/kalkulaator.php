6d220aadc3355838d4a7d83ed2be80335cb4c632
khkmihhail <mihhail.matisinets@khk.ee>
2018-02-08 10:51:39 +0200

    Uus commit. Loonud func.

--- php_praks_1/kalkulaator.php
+++ php_praks_1/kalkulaator.php
@@ -5,13 +5,29 @@
  * Date: 30.01.2018
  * Time: 11:30
  */
+//uus funktsioon. 8.02.2018
+function Hind($taishind, $sooduskart, $kasOledOpilane)
+{
+    $soodustusProts = 15; // %
+    $soodustus = $taishind;
+    $opilaseToetus = 1.80; // eur, float
+    if ($sooduskart) {
+        $soodustus = $taishind * ((100 - $soodustusProts) / 100);
+    }
+    if($kasOledOpilane){
+        $soodustus = $soodustus - $opilaseToetus;
+    }
+    return $soodustus;
+}
+//func call
+Hind()
+
+
 $soogihind = 2.55; // ahjuliha
-$soodustusProts = 15; // %
-$opilaseToetus = 1.80; // eur, float
 $i = 0;
 if($soogihind > 0 and $soodustusProts > 0 and $opilaseToetus > 0) {
-    $soodustus = $soogihind * ((100 - $soodustusProts) / 100);
-    $hindOpilastele = $soogihind - $opilaseToetus;
+
+    $hindOpilastele = $soodustus - $opilaseToetus;
     print("<br><h1>Söögi hind õpilasele $hindOpilastele</h1>");
 }
 
