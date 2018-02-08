ffc585de2f641572011b14647ff7b9934741b269
khkmihhail <mihhail.matisinets@khk.ee>
2018-02-08 12:31:40 +0200

    mitu parandused

--- php_praks_1/ulesanne1_8_02.php
+++ php_praks_1/ulesanne1_8_02.php
@@ -10,30 +10,32 @@ $menu = array(
     array(
       'nimetus' => 'snitsel',
       'kirjeldus' => 'šnitsel sealihast, lisand, kaste, salat, leib',
-      'hind' => 2.68
+      'hind' => '2.68€'
     ),
     array(
         'nimetus' => 'seapraad',
         'kirjeldus' => 'seapraad, lisand, kaste, salat, leib',
-        'hind' => 2.65
+        'hind' => '2.65€'
     ),
     array(
         'nimetus' => 'Hakklihapallid tomatikastmes',
         'kirjeldus' => 'hakklihapall 2 tk, lisand, kaste, salat, leib',
-        'hind' => 2.30
+        'hind' => '2.30€'
     ),
     array(
         'nimetus' => 'Hakklihapallid tomatikastmes 1/2',
         'kirjeldus' => 'hakklihapall 1 tk, lisand, kaste, salat, leib',
-        'hind' => 1.30
+        'hind' => '1.30€'
     ),
 );
 
 //made foreach loop
 foreach ($menu as $praed) {
+    echo '<ul>';
     printf('<br><b>Praed:</b> %s <b>Kirjeldus:</b> %s <b>Hind:</b> %s',
         $praed['nimetus'],$praed['kirjeldus'],$praed['hind']
     );
+    echo'</ul>';
 };
 
 
