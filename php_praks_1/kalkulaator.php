dd9fa7d9512362e181a9781b0ae10dc22d78d84d
khkmihhail <mihhail.matisinets@khk.ee>
2018-02-08 11:39:52 +0200

    Oli loonud trüskklid massivide kasutamiseks.

--- php_praks_1/kalkulaator.php
+++ php_praks_1/kalkulaator.php
@@ -19,12 +19,22 @@ function Hind($taishind, $sooduskart, $kasOledOpilane)
     }
     return $soodustus;
 }
+
+//Massivid
 //loome massivi(arrays)
 //kasutame selleks kujul (sooduskart, kasOledOpilane)
 $opilane = array(true, true); //olemas soodus ja oled Õpilane
 $opetaja = array(true, false);
 $kulaline = array(false, false);
+//loome uks suur array
+//kasutajad on tabel
+$kasutajad = array(
+    array(true, true), //õpilased
+    array(true, false), //õpetajad
+    array(false, false) //külalised
+);
 
+//Testid
 //func call
 $a = Hind(2.65, true, true);
 echo "<br>Hind õpilasele on $a";
@@ -37,4 +47,21 @@ printf("<br>Hind sooduskaardi omanikule %.2f", $a);
 
 $a = Hind(2.65,$opetaja[0],$opetaja[1]);
 printf("<br>Hind sooduskaardi omanikule %.2f", $a);
+
+print ("<br>");
+print ("<br/>Tsükklid testimine:");
+
+//loome foreach ja for trükkelid ja testime tabel.
+for ($i =0; $i < count($kasutajad); $i++){
+    $a = Hind(2.65, $kasutajad[$i][0], $kasutajad[$i][1]);
+    printf("<br>Hind sooduskaardi omanikule %.2f", $a);
+};
+
+foreach ($kasutajad as $value) {
+
+};
+
+
+
+
 ?>
