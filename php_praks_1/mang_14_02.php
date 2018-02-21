1563862b6e18774db836bc823faa6a324acd14e4
khkmihhail <mihhail.matisinets@khk.ee>
2018-02-21 09:29:18 +0200

    Loonud $_SESSEION viisiga

--- php_praks_1/mang_14_02.php
+++ php_praks_1/mang_14_02.php
@@ -7,29 +7,36 @@
  */
 
 require_once 'functsions.php';
+//lisame session
+session_start(); //annab võimalus kasutada $_session
 
 loeVormFailist('vorm2.html');
 
+if (!isset($_SESSION['serveriArv'])){
+    $_SESSION['serveriArv'] = rand(1,50);
+} else {
+    $serveriArv = $_SESSION['serveriArv'];
+}
 
-$arv = $_POST['arv'];
-$juhuslikArv = rand(1,50);
 
 //kas arv on edastatuv
 if(!isset($_POST['arv'])){
     print('Sisesta täisarv.');
 }
-if ($arv > $juhuslikArv){
-    printf("<br>%s on suurem, kui meie arv.", $arv);
+if ($_POST['arv'] >  $_SESSION['serveriArv']){
+    printf("<br>%s on suurem, kui meie arv.", $_POST['arv']);
 }
-if ($arv < $juhuslikArv){
-    printf("<br>%s on väiksem, kui meie arv.", $arv);
+if ($_POST['arv'] <  $_SESSION['serveriArv']){
+    printf("<br>%s on väiksem, kui meie arv.", $_POST['arv']);
 }
-if (abs($arv-$juhuslikArv) <=5){
-    printf("<br>%s on lähedal", $arv);
+if (abs($_POST['arv']- $_SESSION['serveriArv']) <=5){
+    printf("<br>%s on lähedal", $_POST['arv']);
 }
-if ($arv == $juhuslikArv){
-    printf("<br>%s on arvasid ära!", $arv);
+if ($_POST['arv'] ==  $_SESSION['serveriArv']){
+    printf("<br>%s on arvasid ära!", $_POST['arv']);
 }
+
+
 
 
 
